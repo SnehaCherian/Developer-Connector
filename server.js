88006f2559f663c063753b446282ecc6eb75d117
SnehaCherian <snehaacherian@gmail.com>
2020-10-09 19:29:48 +0530

    Making deployment ready

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ app.use('/api/auth',require('./routes/api/auth'));
 app.use('/api/profile',require('./routes/api/profile'));
 app.use('/api/posts',require('./routes/api/posts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
 
 const port=process.env.PORT||5000;
 app.listen(port,()=>console.log(`Server started successfully on port ${port}`));
