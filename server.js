5ed2ab4f3557ea0f0133874c0b73e5c60ed951bb
SnehaCherian <snehaacherian@gmail.com>
2020-10-09 19:00:45 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const app=express();
+const connectDB=require(__dirname+"/config/db.js");
+
+//Connect Database
+connectDB();
+//Initialize middleware
+app.use(express.json({extended:false}));
+
+//app.get("/",(req,res)=>res.send("API running"));
+
+//Define routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
+
+
+const port=process.env.PORT||5000;
+app.listen(port,()=>console.log(`Server started successfully on port ${port}`));
